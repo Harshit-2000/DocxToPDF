179247ac153b2f478336b822d95250ba40911aab
Harshit Singh <hrshit2000@gmail.com>
2021-12-16 22:55:32 +0530

    change dependices

--- convert.py
+++ convert.py
@@ -2,7 +2,6 @@ from docx2pdf import convert
 import time
 import sys
 import os
-import comtypes.client
 
 
 class Convert:
@@ -16,24 +15,6 @@ class Convert:
         if ext == 'docx':
             try:
                 convert('input/', 'output/')
-            except Exception as e:
-                print(e)
-        elif ext == 'doc':
-            try:
-                wdFormatPDF = 17
-                BASE = os.path.abspath(os.getcwd())
-                inFile = os.path.join(BASE, 'input', filename)
-                outFile = os.path.join(BASE, 'output', filename)
-
-                print(os.path.abspath(sys.argv[1]))
-                print(os.path.abspath(sys.argv[2]))
-
-                word = comtypes.client.CreateObject('Word.Application')
-                doc = word.Documents.Open(inFile)
-                doc.SaveAs(outFile, FileFormat=wdFormatPDF)
-
-                doc.Close()
-                word.Quit()
             except Exception as e:
                 print(e)
         else:
